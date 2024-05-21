d6652dc222d95a259e290a22f4fa7a5077fbc0c9
Pavle Kraljic <kraljicpavle9@gmail.com>
2024-05-21 23:19:43 +0200

    added event listener for password check input

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 
-
 document.getElementById('btn').addEventListener('click', () => {
-        const passwordInput = document.getElementById('password-input');
-        const passwordCheck = document.getElementById('password-check');
+    const passwordInput = document.getElementById('password-input');
         if (passwordInput.value === '') {
             passwordInput.classList.add('error')
         } else {
             passwordInput.classList.add('accept')
         }
         });
+
+
+        document.getElementById('btn').addEventListener('click', () => {
+            const passwordCheck = document.getElementById('password-check');
+            if (passwordCheck.value === '') {
+                passwordCheck.classList.add('error')
+            } else {
+                passwordCheck.classList.add('accept')
+            }
+        });
+
 
 
